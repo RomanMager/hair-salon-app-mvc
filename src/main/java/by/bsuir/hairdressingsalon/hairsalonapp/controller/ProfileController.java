b1ad11633e8c973ea8c90476fd0d29785b0df172
Liza <Lizadobrogost@outlook.com>
2020-12-08 02:01:47 +0300

    Tried to make edit profile button:((

--- src/main/java/by/bsuir/hairdressingsalon/hairsalonapp/controller/ProfileController.java
+++ src/main/java/by/bsuir/hairdressingsalon/hairsalonapp/controller/ProfileController.java
@@ -1,9 +1,6 @@
 package by.bsuir.hairdressingsalon.hairsalonapp.controller;
 
-import by.bsuir.hairdressingsalon.hairsalonapp.entity.Customer;
-import by.bsuir.hairdressingsalon.hairsalonapp.entity.Employee;
-import by.bsuir.hairdressingsalon.hairsalonapp.entity.ProcedureAppointment;
-import by.bsuir.hairdressingsalon.hairsalonapp.entity.SalonProcedure;
+import by.bsuir.hairdressingsalon.hairsalonapp.entity.*;
 import by.bsuir.hairdressingsalon.hairsalonapp.service.AppointmentService;
 import by.bsuir.hairdressingsalon.hairsalonapp.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +8,12 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/profile")
@@ -36,5 +36,19 @@ public class ProfileController {
         model.addAttribute("appointmentsForCustomer", appointmentsForCustomer);
 
         return "customer/profile";
+    }
+
+    @PostMapping("/profile")
+    public String updateProfile(@PathVariable Long id, @AuthenticationPrincipal Customer customer, Model model) {
+        customer.setId(id);
+        customerService.save(customer);
+
+        return "redirect:/profile";
+    }
+
+    @GetMapping("/employees/edit/{id}")
+    public String showEmployeeEditForm(@PathVariable Long id, Model model) {
+
+        return "customer/profile-edit";
     }
 }
